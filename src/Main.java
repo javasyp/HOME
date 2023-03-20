0aeebead44a68a0f39aeed1e2377b0f7f18edc3e
shinyoung <tlsdud625@naver.com>
2023-03-20 16:52:26 +0900

    ArrayList 내에서 특정 게시물 찾는 메소드 구현(foreach)

--- src/Main.java
+++ src/Main.java
@@ -147,21 +147,29 @@ public class Main {
 	}
 
 	private static int getArticleIndexById(int id) {
-		for (int i = 0; i < articles.size(); i++) {
-			Article article = articles.get(i);
+		int i = 0;
+		
+		for (Article article : articles) {
 			if (article.id == id) {
 				return i;
 			}
+			i++;
 		}
 		return -1;
 	}
 
 	private static Article getArticleById(int id) {
-		for (int i = 0; i < articles.size(); i++) {
-			Article article = articles.get(i);
-			if (article.id == id) {
-				return article;
-			}
+//		for (Article article : articles) {
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+//		return null;
+		
+		int index = getArticleIndexById(id);
+
+		if (index != -1) {
+			return articles.get(index);
 		}
 		return null;
 	}
