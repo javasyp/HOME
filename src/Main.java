4359e59b8768d8fdd7001a0a3f97392a08e92d31
shinyoung <tlsdud625@naver.com>
2023-03-20 15:06:37 +0900

    게시글 작성 시 날짜와 시간 정보 저장

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
 			String command = sc.nextLine().trim();
 			
 			if (command.length() == 0) {
-				System.out.println("명령어를 입력해주세요");
+				System.out.println("명령어를 입력해 주세요.");
 				continue;
 			}
 			
@@ -41,13 +41,15 @@ public class Main {
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				
+				String regDate = Util.getNowDateTimeStr();
+				
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -79,10 +81,10 @@ public class Main {
 					continue;
 				} 
 				
-				System.out.printf("번호 : %d", foundArticle.id);
-				System.out.printf("날짜 : %s", "2023-12-12 12:12:12");
-				System.out.printf("제목 : %s", foundArticle.title);
-				System.out.printf("내용 : %s", foundArticle.body);
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
 				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
@@ -97,11 +99,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	Article(int id, String title, String body) {
+	Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
